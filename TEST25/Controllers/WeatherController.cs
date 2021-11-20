0802e0614af121f7cdd3b82f422f0cfae3e1dc49
YanivBohbot <Yanivbohbot5@gmail.com>
2021-11-20 19:19:33 +0200

    Add project files.

---
+++ TEST25/Controllers/WeatherController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TEST25.Models;
+using TEST25.Services;
+
+namespace TEST25.Controllers
+{
+    [ApiController]
+    [Route("Weather")]
+    public class WeatherController : ControllerBase
+    {
+
+        
+        private readonly IWeatherService _weatherService;
+
+
+        public WeatherController(IWeatherService weatherService)
+        {
+
+            _weatherService = weatherService;
+
+        }
+
+
+        [HttpGet]
+        [Route("auto/{city_name}")]
+        public async Task<string> GetAutoComplete(string city_name)
+        {
+
+           return await _weatherService.GetResult(city_name);
+
+        }
+
+
+        [HttpGet]
+        [Route("current/{key_city}")]
+        public async Task<string> GetCurrentCondition(string key_city)
+        {
+
+            return await _weatherService.GetCurrentWeather(key_city);
+
+        }
+
+    }
+}
